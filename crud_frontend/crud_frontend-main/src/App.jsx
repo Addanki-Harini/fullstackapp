8200c6b339538bbc1941485647921fe830371a4e
Addanki-Harini <addankiharini9@gmail.com>
2025-09-08 12:44:38 +0530

    Update App.jsx

--- crud_frontend/crud_frontend-main/src/App.jsx
+++ crud_frontend/crud_frontend-main/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://184.72.122.226:9090/springapp1';
+const BASE_URL = 'http://67.202.45.198:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
